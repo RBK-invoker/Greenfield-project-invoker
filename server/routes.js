ade19f14b482d9e1a0e1f445546c3fea44e5f701
Belalfaouri <belalfaourig@gmail.com>
2018-04-21 15:07:26 +0300

    fixed syntax

--- server/routes.js
+++ server/routes.js
@@ -1,33 +1,33 @@
 var router=require('express').Router();
-var controller=require('../database-mongo/PatientController');
+var controller=require('../database-mongo/PatientController.js');
 //routes and handling requests.
 
 router.route('/').get(function(req,res){
   res.send('hi')
 })
-
-router.route('/login').get()
-
-router.route('/login').post()
-
-router.route('/signup').get()
-
-router.route('/signup').post()
+//
+// router.route('/login').get()
+//
+// router.route('/login').post()
+//
+// router.route('/signup').get()
+//
+// router.route('/signup').post()
 
 //retrieve a pateint.
-router.route('/patient').get(controller)
+router.route('/patient').get(controller.retrieveOne)
 
 //create a patient.
-router.route('/patient').post(controller)
+router.route('/patient').post(controller.createOne)
 
 //update patient information.
-router.route('/patient').put(controller)
+router.route('/patient').put(controller.updateOne)
 
 //delete a patient.
-router.route('/patient').delete(controller)
+router.route('/patient').delete(controller.delete)
 
 //get all patients
-router.route('/patients').get(controller)
+router.route('/patients').get(controller.retrieveAll)
 
 
 
