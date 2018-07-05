390d2539e4100f29020ce165691d8361c5f16d8d
Salsabeel <s.jaafreh@yahoo.com>
2018-07-05 16:28:04 +0300

    newpatient styling

--- react-client/src/pages/NewPatient.jsx
+++ react-client/src/pages/NewPatient.jsx
@@ -1,9 +1,9 @@
-//to work react
+
 import React from 'react';
-//to work in ajax
+
 import $ from 'jquery';
-//syle for div to arrange thing
-const main={
+//styling part
+const main = {
   width: '400px',
   height: '150px',
   display: '-webkit-flex',
@@ -11,33 +11,32 @@ const main={
   marginBottom:'-50px',
   marginLeft:'7%',
 }
-//the style for the main header
-const header={
+
+const header = {
   color:'black',
   fontWeight:'bold',
   textAlign:'center',
   fontSize:'40px',
   fontFamily: 'Lobster',
 };
-//the style for number
-const number={
+
+const number = {
   color:'black',
   fontWeight:'bold',
   textAlign:'center',
   fontSize:'25px',
   marginRight:'20px',
 };
-//the style for number2
-const number2={
+
+const number2 = {
   color:'black',
   fontWeight:'bold',
   textAlign:'center',
   fontSize:'25px',
 };
-//the style for the user and password input
-const input={
+
+const input = {
   padding: '10px 10px 10px 10px',
-  //this three to make it center
   display: 'block',
   marginRight: 'auto',
   marginLeft: 'auto',
@@ -47,9 +46,8 @@ const input={
   borderRadius: '15px',
 };
 //the style for the button Sign Up
-const button={
+const button = {
   padding:'5px',
-  //this three to make it center
   display: 'block',
   marginRight: 'auto',
   marginLeft: 'auto',
@@ -61,13 +59,13 @@ const button={
   borderRadius: '10px',
   fontFamily: 'Lobster',
 };
-//the page login what inside render
+
+
 class NewPatient extends React.Component {
-  //constructor to undestand state
   constructor(props){
     super(props);
     //all the data save before sent in state
-    this.state={
+    this.state = { 
       number: 0,
       firstName:'',
       lastName: '',
@@ -83,89 +81,85 @@ class NewPatient extends React.Component {
       loggedIn:true,
     };
   }
-  //for home button
+
   home(){
     window.location.href= window.location.origin+'/'
   };
-  //when change  ... change the
-  //number
+
   onWrite1 (e) {
     this.setState({
       number: 1*e.target.value,
     });
-    console.log(this.state.number,typeof this.state.number)
   };
-  //first name
+
+
   onWrite2 (e) {
     this.setState({
       firstName: e.target.value,
     });
   };
-  //last name
+
   onWrite3 (e) {
     this.setState({
       lastName: e.target.value,
     });
   };
-  //gender
+
   onWrite4 (e) {
     this.setState({
       gender: e.target.value,
     });
   };
-  //age
+
   onWrite5 (e) {
     this.setState({
       age: e.target.value,
     });
   };
-  //phone
+
   onWrite6 (e) {
     this.setState({
       phone: e.target.value,
     });
   };
-  //conditions
+
   onWrite7 (e) {
     this.setState({
       conditions: e.target.value,
     });
   };
-  //past_Diseases
+
   onWrite8 (e) {
     this.setState({
       past_Diseases: e.target.value,
     });
   };
-  //currentlly_Medications
+
   onWrite9 (e) {
     this.setState({
       currentlly_Medications: e.target.value,
     });
   };
-  //genetic_Diseases
+
   onWrite10 (e) {
     this.setState({
       genetic_Diseases: e.target.value,
     });
   };
-  //allergies
+
   onWrite11 (e) {
     this.setState({
       allergies: e.target.value,
     });
   };
-  //description
+
   onWrite12 (e) {
     this.setState({
       description: e.target.value,
     });
   };
-  //for send data in button
+
   sentData() {
-    //${JSON.stringify(this.state)}
-    console.log(`you sent this data: ${typeof this.state.number} ${this.state.number}`);
-    //ajax request to sent the data to server then data base
     $.ajax({
       type: 'POST',
       url: '/patient',
@@ -186,56 +180,53 @@ class NewPatient extends React.Component {
       //when success do this
       success: function (res) {
         alert(res);
-        console.log(res);
       },
-      //when error do this
       error: function (res){
         alert('Failed sent this data please try agian' );
-        console.log('Failed sent this data please try agian')
       },
     });
   };
-  //what render
+
   render () {
     return (
-      <div style={{textAlign: 'center'}}>
-        <h2 style={header}>Welcome To Medical Record <br/> Please Insert Your Information</h2>
-        <div1 style={main}>
-          <h3 className="column" style={number}>
+      <div style = {{textAlign: 'center'}}>
+        <h2 style = {header}>Welcome To Medical Record <br/> Please Insert Your Information</h2>
+        <div1 style = {main}>
+          <h3 className = "column" style = {number}>
           Number:
-            <input type="number" value={this.state.number} onChange={this.onWrite1.bind(this)} placeholder="Number" style={input}>
+            <input type = "number" value = {this.state.number} onChange = {this.onWrite1.bind(this)} placeholder = "Number" style = {input}>
             </input>
           </h3>
-          <h3 style={number}>
+          <h3 style = {number}>
           First name:
-            <input value={this.state.firstName} onChange={this.onWrite2.bind(this)} placeholder="First name" style={input}>
+            <input value = {this.state.firstName} onChange = {this.onWrite2.bind(this)} placeholder = "First name" style = {input}>
             </input>
           </h3>
-          <h3 style={number}>
+          <h3 style =  {number}>
           Last name:
-            <input value={this.state.lastName} onChange={this.onWrite3.bind(this)} placeholder="Last name" style={input}>
+            <input value = {this.state.lastName} onChange = {this.onWrite3.bind(this)} placeholder = "Last name" style = {input}>
             </input>
           </h3>
-          <h3 style={number2}>
+          <h3 style = {number2}>
           Gender:
-            <input value={this.state.gender} onChange={this.onWrite4.bind(this)} placeholder="Gender" style={input}>
+            <input value = {this.state.gender} onChange = {this.onWrite4.bind(this)} placeholder = "Gender" style = {input}>
             </input>
           </h3>
         </div1>
-        <div2 style={main}>
-          <h3 style={number}>
+        <div2 style = {main}>
+          <h3 style = {number}>
           Age:
-            <input type="number" value={this.state.age} onChange={this.onWrite5.bind(this)} placeholder="Age" style={input}>
+            <input type = "number" value = {this.state.age} onChange = {this.onWrite5.bind(this)} placeholder = "Age" style = {input}>
             </input>
           </h3>
-          <h3 style={number}>
+          <h3 style = {number}>
           Phone:
-            <input type="number" value={this.state.phone} onChange={this.onWrite6.bind(this)} placeholder="Phone" style={input}>
+            <input type = "number" value = {this.state.phone} onChange = {this.onWrite6.bind(this)} placeholder = "Phone" style = {input}>
             </input>
           </h3>
-          <h3 style={number}>
+          <h3 style = {number}>
           Conditions:
-            <input value={this.state.conditions} onChange={this.onWrite7.bind(this)} placeholder="Conditions" style={input}>
+            <input value = {this.state.conditions} onChange = {this.onWrite7.bind(this)} placeholder = "Conditions" style = {input}>
             </input>
           </h3>
           <h3 style={number2}>
@@ -244,33 +235,34 @@ class NewPatient extends React.Component {
             </input>
           </h3>
         </div2>
-        <div3 style={main}>
-          <h3 style={number}>
+        <div3 style = {main}>
+          <h3 style = {number}>
           Curr. Medications:
-            <input value={this.state.currentlly_Medications} onChange={this.onWrite9.bind(this)} placeholder="Currentlly Medications" style={input}>
+            <input value = {this.state.currentlly_Medications} onChange = {this.onWrite9.bind(this)} placeholder = "Currentlly Medications" style = {input}>
             </input>
           </h3>
-          <h3 style={number}>
+          <h3 style = {number}>
           Genetic Diseases:
-            <input value={this.state.genetic_Diseases} onChange={this.onWrite10.bind(this)} placeholder="Genetic Diseases" style={input}>
+            <input value = {this.state.genetic_Diseases} onChange = {this.onWrite10.bind(this)} placeholder = "Genetic Diseases" style = {input}>
             </input>
           </h3>
           <h3 style={number}>
           Allergies:
-            <input value={this.state.allergies} onChange={this.onWrite11.bind(this)} placeholder="Allergies" style={input}>
+            <input value = {this.state.allergies} onChange = {this.onWrite11.bind(this)} placeholder = "Allergies" style = {input}>
             </input>
           </h3>
           <h3 style={number2}>
-          Description:
-            <input value={this.state.description} onChange={this.onWrite12.bind(this)} placeholder="Description" style={input}>
+          Description: 
+            <input value = {this.state.description} onChange = {this.onWrite12.bind(this)} placeholder = "Description" style = {input}>
             </input>
           </h3>
         </div3>
-        <button onClick={this.sentData.bind(this)} style={button}>Submit</button>
-        <button onClick={this.home.bind(this)} style={button}>Back to Home</button>
+        <button onClick = {this.sentData.bind(this)} style = {button}>Submit</button>
+        <button onClick = {this.home.bind(this)} style = {button}>Back to Home</button>
       </div>
     )
   }
 }
+
 //export this component to can use
 export default NewPatient;
