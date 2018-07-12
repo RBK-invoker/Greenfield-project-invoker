f786f1ed7ba98de7a347ac35039be218ddb86743
MohammadAlRifai <mohammad94sar@gmail.com>
2018-07-12 16:18:59 +0300

    Edit code style

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var router = require("./routes.js");
-var session = require("express-session");
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = require('./routes.js');
+var session = require('express-session');
 
 var app = express();
 
 //adding express-session to our app.
 app.use(
-  session({
-    secret: "123456",
-    resave: false,
-    saveUninitialized: true
-  })
+	session({
+		secret: '123456',
+		resave: false,
+		saveUninitialized: true
+	})
 );
 
 //using router for the request handling with all routes defined inside.
-app.use("/", router);
+app.use('/', router);
 
-app.use(express.static(__dirname + "/../react-client/dist"));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("listening on port: ", port);
+	console.log('listening on port: ', port);
 });
